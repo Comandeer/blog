8d28b49565440c52eee2ab18151833b2e6bc3cba
Comandeer <comandeer@comandeer.pl>
2024-05-26 00:43:36 +0200

    Setup 11ty.

---
+++ .eleventy.js
@@ -0,0 +1,14 @@
+/* global module */
+
+/**
+ *
+ * @param {import('@11ty/eleventy').UserConfig} eleventyConfig
+ * @returns
+ */
+module.exports = function( eleventyConfig ) {
+	return {
+		dir: {
+			layouts: '_layouts'
+		}
+	};
+};
