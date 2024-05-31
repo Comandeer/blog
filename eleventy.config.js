ba2b35ff9413c502a8d8cc89f4141a3ed45a7839
Comandeer <comandeer@comandeer.pl>
2024-06-01 01:18:32 +0200

    Move MD parser to a separate file.

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,14 +2,9 @@
 
 const path = require( 'node:path' );
 const { formatRFC3339 } = require( 'date-fns' );
-const markdownIt = require( 'markdown-it' );
-const markdownItAnchor = require( 'markdown-it-anchor' );
-const markdownItClass = require( '@toycode/markdown-it-class' );
-const markdownItLinkAttributes = require( 'markdown-it-link-attributes' );
-const markdownItEmoji = require( 'markdown-it-emoji' ).full;
 const sass = require( 'sass' );
-const slugify = require( 'slugify' );
 const site = require( './src/_data/site' );
+const markdownIt = require( './plugins/markdownIt' );
 const imageShortCode = require( './shortcodes/image' );
 
 /**
@@ -19,79 +14,7 @@ const imageShortCode = require( './shortcodes/image' );
 module.exports = function( eleventyConfig ) {
 	eleventyConfig.ignores.add( '/images/**' );
 
-	const placeholder = '™™©©®®';
-	slugify.extend( {
-		'ę': 'ę',
-		'Ę': 'ę',
-		'ó': 'ó',
-		'Ó': 'ó',
-		'ą': 'ą',
-		'Ą': 'ą',
-		'ś': 'ś',
-		'Ś': 'ś',
-		'ł': 'ł',
-		'Ł': 'ł',
-		'ż': 'ż',
-		'Ż': 'ż',
-		'ź': 'ź',
-		'Ź': 'ź',
-		'ć': 'ć',
-		'Ć': 'ć',
-		'ń': 'ń',
-		'Ń': 'ń',
-		'[': '',
-		']': '',
-		'/': '',
-		'–': placeholder,
-		'—': placeholder,
-		'+': placeholder
-	} );
-
-	eleventyConfig.setLibrary( 'md', markdownIt ( {
-		html: true,
-		breaks: true,
-		linkify: true,
-		typographer: true
-	} ).use( markdownItAnchor, {
-		slugify: ( str ) => {
-			str = str.
-				replaceAll( ' ', '' ).
-				replaceAll( '– ', ' ' );
-
-			const slug = slugify( str, {
-				replacement: '-',
-				remove: /[#,&,+()$~%.'":*¿?¡!<>{}]/g,
-				lower: true,
-				trim: false
-			} );
-
-			return slug.
-				replaceAll( placeholder, '' ).
-				replaceAll( '👍', '' ).
-				replaceAll( '👎', '' );
-		},
-		tabIndex: false,
-		permalink: markdownItAnchor.permalink.headerLink()
-	} ).
-		use( markdownItClass, {
-			h1: [ 'title', 'is-1' ],
-			h2: [ 'title', 'is-2' ],
-			h3: [ 'title', 'is-3' ],
-			h4: [ 'title', 'is-4' ],
-			h5: [ 'title', 'is-5' ],
-			h6: [ 'title', 'is-6' ]
-		} ).
-		use( markdownItLinkAttributes, [
-			{
-				matcher( href ) {
-					return !href.startsWith( 'https://blog.comandeer.pl' ) && href.match( /^https?:\/\// );
-				},
-				attrs: {
-					rel: 'noreferrer noopener'
-				}
-			}
-		] ).
-		use( markdownItEmoji ) );
+	eleventyConfig.setLibrary( 'md', markdownIt );
 
 	eleventyConfig.addCollection( 'posts', ( collection ) => {
 		return collection.getFilteredByGlob( 'src/_posts/**/*.md' ).
---
+++ plugins/markdownIt.js
@@ -0,0 +1,81 @@
+const markdownIt = require( 'markdown-it' );
+const markdownItAnchor = require( 'markdown-it-anchor' );
+const markdownItClass = require( '@toycode/markdown-it-class' );
+const markdownItLinkAttributes = require( 'markdown-it-link-attributes' );
+const markdownItEmoji = require( 'markdown-it-emoji' ).full;
+const slugify = require( 'slugify' );
+
+const SLUG_PLACEHOLDER = '™™©©®®';
+
+slugify.extend( {
+	'ę': 'ę',
+	'Ę': 'ę',
+	'ó': 'ó',
+	'Ó': 'ó',
+	'ą': 'ą',
+	'Ą': 'ą',
+	'ś': 'ś',
+	'Ś': 'ś',
+	'ł': 'ł',
+	'Ł': 'ł',
+	'ż': 'ż',
+	'Ż': 'ż',
+	'ź': 'ź',
+	'Ź': 'ź',
+	'ć': 'ć',
+	'Ć': 'ć',
+	'ń': 'ń',
+	'Ń': 'ń',
+	'[': '',
+	']': '',
+	'/': '',
+	'–': SLUG_PLACEHOLDER,
+	'—': SLUG_PLACEHOLDER,
+	'+': SLUG_PLACEHOLDER
+} );
+
+module.exports = markdownIt ( {
+	html: true,
+	breaks: true,
+	linkify: true,
+	typographer: true
+} ).use( markdownItAnchor, {
+	slugify: ( str ) => {
+		str = str.
+			replaceAll( ' ', '' ).
+			replaceAll( '– ', ' ' );
+
+		const slug = slugify( str, {
+			replacement: '-',
+			remove: /[#,&,+()$~%.'":*¿?¡!<>{}]/g,
+			lower: true,
+			trim: false
+		} );
+
+		return slug.
+			replaceAll( SLUG_PLACEHOLDER, '' ).
+			replaceAll( '👍', '' ).
+			replaceAll( '👎', '' );
+	},
+	tabIndex: false,
+	permalink: markdownItAnchor.permalink.headerLink()
+} ).
+	use( markdownItClass, {
+		h1: [ 'title', 'is-1' ],
+		h2: [ 'title', 'is-2' ],
+		h3: [ 'title', 'is-3' ],
+		h4: [ 'title', 'is-4' ],
+		h5: [ 'title', 'is-5' ],
+		h6: [ 'title', 'is-6' ]
+	} ).
+	use( markdownItLinkAttributes, [
+		{
+			matcher( href ) {
+				return !href.startsWith( 'https://blog.comandeer.pl' ) && href.match( /^https?:\/\// );
+			},
+			attrs: {
+				rel: 'noreferrer noopener'
+			}
+		}
+	] ).
+	use( markdownItEmoji );
